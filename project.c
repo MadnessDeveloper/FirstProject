90305ddbb09f35dbf55946546143e9dc2686f31d
MadnessDeveloper <m.aminb@ymail.com>
2017-05-13 10:44:16 +0430

    commitedSuccessfully

---
+++ project.c
@@ -0,0 +1,17 @@
+/*
+ * project.c
+ *
+ * Created: 4/15/2016 4:38:28 PM
+ * Author: Amin
+ */
+
+#include <io.h>
+
+void main(void)
+{
+while (1)
+    {
+    // Please write your application code here
+
+    }
+}
